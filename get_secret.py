ce3a0ad756e452d8106087bfd573f5eb28351540
geek3000 <58487765+geek3000@users.noreply.github.com>
2022-02-17 11:07:19 +0100

    Update get_secret.py

--- get_secret.py
+++ get_secret.py
@@ -10,10 +10,10 @@ headers = {'Content-type': 'application/json'}
 
 print("Get Openid Provider info")
 
-if True:
+try:
     client_info=r.post('https://iddev.fedorainfracloud.org/openidc/Registration', data=json.dumps(submit_info), headers=headers).json()
     print(client_info)
     with open('client_secrets.json', 'w') as f:
         f.write(json.dumps(client_info))
-else:
+catch:
     print("Cant get credentials!")
